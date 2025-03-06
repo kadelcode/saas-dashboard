eef335ebb11d972f89c1d8cd6709c5496d2ec79c
kadelcode <kadelcode@gmail.com>
2025-03-06 10:04:07 +0100

    Add password and confirmpassword fields with logic

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -8,10 +8,16 @@ const Register = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [name, setName] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
     const router = useRouter();
 
     const handleRegister = async (e: React.FormEvent) => {
         e.preventDefault(); // Prevents the default form submission behavior
+
+        // Checks for matching password
+        if (password !== confirmPassword) {
+            alert("Passwords do not match!");
+        }
         await registerUser(email, password, name);
         router.push("/")
     };
@@ -19,7 +25,7 @@ const Register = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h2 className="text-2xl font-bold">Register</h2>
-            <form onSubmit={handleRegister} className="flex flex-col gap-4 mt-4">
+            <form onSubmit={handleRegister} className="flex flex-col gap-4 mt-4 max-w-96">
                 <input
                   type="text"
                   placeholder="Name"
@@ -31,7 +37,23 @@ const Register = () => {
                   type="email"
                   placeholder="Email"
                   value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="border p-2 rounded"
+                />
+
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
                   onChange={(e) => setPassword(e.target.value)}
+                  className="border p-2 rounded"
+                />
+
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
                   className="border p-2 rounded"
                 />
                 <button type="submit" className="bg-blue-600 text-white p-2 rounded">Register</button>
