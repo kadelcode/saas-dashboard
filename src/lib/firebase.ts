746a20daa38f3462a48d74a765941d40d245561d
kadelcode <kadelcode@gmail.com>
2025-03-06 09:27:08 +0100

    Update code for firebase setup

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,25 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
-import firebase from "firebase/compat/app";
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "NEXT_API_KEY",
-    authDomanin: "NEXT_AUTH_DOMAIN",
-    projectId: "NEXT_PROJECT_ID",
-    storageBucket: "NEXT_STORAGE_BUCKET",
-    messageSenderId: "NEXT_MESSAGING_SENDER_ID",
-    appId: "NEXT_APP_ID",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomanin: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messageSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+/* Ensures that Firebase is initialized only once, preventing errors. */
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+const analytics = getAnalytics(app);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
