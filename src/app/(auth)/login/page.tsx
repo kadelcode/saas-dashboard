e81fc5b4d57e88da95422f83f9ec7ea35198ef2b
kadelcode <kadelcode@gmail.com>
2025-03-10 20:15:45 +0100

    Remove unused components

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -17,10 +17,10 @@ import { useRouter } from "next/navigation";
 import { ClipLoader } from "react-spinners";
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import toast from "react-hot-toast";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
+// import { Button } from "@/components/ui/button";
 
 // Defines a functional component
 const Login = () => {
